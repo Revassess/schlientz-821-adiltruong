200c26c5c79f4143e1ef9a8fede50c042d0543fe
Adil <adiltruong@umail.ucsb.edu>
2020-08-04 11:36:07 -0700

    finished 1 and 4 of tier1

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,14 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
-		return false;
+		String ln = Long.toString(num);
+		int len = ln.length();
+		long res = 0;
+
+		for (int i=0; i < len; i++){
+			res += Math.pow(Character.getNumericValue(ln.charAt(i)),len);
+		}
+
+		return res == num;
 	}
 }
